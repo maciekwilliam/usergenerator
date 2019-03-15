90d1d40b13314060fe2e1a87e734ba3a91f5a260
maciekwilliam <maciejwillem@gmail.com>
2019-03-15 18:21:20 +0100

    Updates

---
+++ precache-manifest.c42d483bfd86c6c3dd40ab9f86e69e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cef40424f4ef85cb100",
+    "url": "/usergenerator/static/js/runtime~main.3cef4042.js"
+  },
+  {
+    "revision": "00c2cf9256c6867dce3e",
+    "url": "/usergenerator/static/js/main.00c2cf92.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/usergenerator/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "00c2cf9256c6867dce3e",
+    "url": "/usergenerator/static/css/main.d77e2989.chunk.css"
+  },
+  {
+    "revision": "407bf2f7d5ba5881c0845add746915de",
+    "url": "/usergenerator/index.html"
+  }
+];
